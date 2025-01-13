9b64125fafb4cd3ff4fe69191c3f5ed680550292
mayuri <mayuri@gmail.com>
2025-01-13 13:29:17 +0530

    test

---
+++ thymeleafExample/startup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+  #cd /home/admin123/themeleaf/thymeleafExample/
+  sudo apt install openjdk-8-jdk -y
+  sudo apt-get install maven -y 
+  mvn test
+  mvn package
+  #mvn clean update 
+  #mvn install
+  #cd /home/admin123/themeleaf/
+  #chmod 700 startup.sh
+  cd target
+  sudo nohup java -jar work-0.0.1-SNAPSHOT.jar >/tmp/app.log2>&1 &
